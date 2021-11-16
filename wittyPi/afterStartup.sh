0575e233c292a2df12e3942dd9851da5efae2631
Guilhem A <guilhem.a@free.fr>
2021-11-16 14:20:03 +0000

    wittypi files

---
+++ wittyPi/afterStartup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# file: afterStartup.sh
+#
+# This script will be executed in background after Witty Pi 3 gets initialized.
+# If you want to run your commands after boot, you can place them here.
+#
+
+#
+# WiiBee mount and autorun USB
+#
+USB_DEV=/dev/sda1
+USB_MNT=/mnt/bee1
+logger "Check if USB disk $USB_DEV is plugged in"
+[ -e $USB_DEV ] && logger "mount $USB_DEV" || exit 1
+#sudo mount -o uid=pi,gid=pi -o umask=000 $USB_DEV $USB_MNT
+sudo mount $USB_DEV $USB_MNT
+SCRIPT="autorun.sh"
+USB_DIR="${USB_MNT}/wiibee"
+[ -x "${USB_DIR}/${SCRIPT}" ] || exit 2
+
+cd $USB_DIR
+# TODO if we have connection, if update enabled, update scripts
+# wget -q --spider http://pierriko.com/wiibee/update
+# git fetch origin && git merge origin/master
+
+
+#export WIIBEE_SHUTDOWN=1
+WIIBEE_SHUTDOWN=1
+env | grep '^WIIBEE_SHUTDOWN='
+#export PATH="${USB_DIR}:${PATH}"
+PATH="${USB_DIR}:${PATH}"
+env | grep '^PATH='
+echo $WIIBEE_SHUTDOWN
+echo $PATH
+
+
+. $SCRIPT 2>> autorun.log
---
+++ wittyPi/beforeShutdown.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# file:beforeShutdown.sh
+
+
+#
+# Save and commit wittypi log files
+#
+#USB_WPi=/mnt/bee1/wiibee/wittyPi
+#WIT_DIR=~/wittypi
+#[ -e $USB_WPi ] || exit 1
+
+#cp ${WIT_DIR}/schedule.log ${USB_WPi}/
+#cp ${WIT_DIR}/wittyPi.log ${USB_WPi}/
+
+#cd ${USB_WPi}/..
+
+#sudo git add .
+#git commit wittyPi/schedule.log -m"[log] $(date -Is)"
+#git commit wittyPi/wittyPi.log -m"[log] $(date -Is)"
+#git push
