558732c8aaf3ef488be8fc201aabcd4b0d1b72a5
Guilhem A <guilhem.a@free.fr>
2023-12-09 16:14:32 +0000

    remove HDMI stuff

--- autorun.sh
+++ autorun.sh
@@ -12,11 +12,6 @@ BTRLADDR="4F:F8:09:01:65:00"
 # switch off WIFI
 sudo modprobe -rv brcmfmac
 echo "WIFI switched-off"
-# Turn off HDMI output
-sudo /opt/vc/bin/tvservice -o
-# Turn it back on again
-#sudo /opt/vc/bin/tvservice -p
-
 
 # Connexion cle 3G
 # fix Huawei E3531 recognized as CDROM [sr0]
